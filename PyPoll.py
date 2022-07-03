23a51b11cce0619a0ccdeb577c94690d0393d500
C-Herr <corey.p.herring@gmail.com>
2022-07-02 21:44:18 -0400

    Election audit results cleaned up

--- PyPoll.py
+++ PyPoll.py
@@ -1,41 +1,25 @@
-# The data we need to retrieve.
-# 1. The total number of votes cast
-# 2. A complete list of candiates who recieved votes
-# 3. The percentage of votes each candidate won
-# 4. The total number of votes each candidate won
-# 5. The winner of the election based on popular vote
-
-#Resources/election_results.csv
-
-#Import the datetime class from the datetime module.
-#import datetime as dt
 #importing the csv file 
 import os
 import csv 
 import sys
 
 #Assign a variable to load a file from a path.
-#file_to_load = ('./Resources/election_results.csv')
-
-#election_data = open(file_to_load,'r')
-#print(election_data)
-
-# To do: perform analysis
-
-#Close the file. 
-#election_data.close()
-#file_to_load = os.path.join('.','Resources','election_results.csv')
-
-#Assign a variable to load a file from a path.
 file_to_load = os.path.join('./Resources','election_results.csv')
-
-#file_to_load = os.path.join(os.path.sep,'Users','dominiqueherring','Desktop','Analysis Projects','Election_Analysis-','Resources','election_results.csv')
 
 #Assign a variable to save the file to a path.
 #file_to_save = os.path.join('.','analysis','election_analysis.txt')
 file_to_save = os.path.join('./analysis','election_analysis.txt')
 
+# Increment each vote by 1
+total_votes = 0 
+#Candidate options and candidate votes
+candintes_options = []
+candidates_votes = {}
 
+# Track the winning candidate, vote count, and percentage.
+winning_candidate = ''
+winning_count = 0
+winning_percentage = 0
 
 #Open the election results and read the file.
 #with open(file_to_save,'w') as txt_file:
@@ -43,54 +27,55 @@ file_to_save = os.path.join('./analysis','election_analysis.txt')
 with open(file_to_load) as election_data:
 
     #TO Do: read and analyze data here.
-  file_reader = csv.reader(election_data)
+    file_reader = csv.reader(election_data)
 
 # Print the header row.
-  headers = next(file_reader)
-  print(headers)
+    headers = next(file_reader)
+  #print(headers)
 
+#Print each row in csv file
+    for row in file_reader:
+      #print(row)
+      total_votes +=1
+    
+    #Print the candidates name from each row
+      candidates_names = row[2]
+    
+     #If the candidate does not match any existing candidate add it the candidate list.
+      if candidates_names not in candintes_options:
+    
+    # Add the candidate name to the cadidate list  
+        candintes_options.append(candidates_names)
+    
+    #Begin tracking that candidate's vote count
+        candidates_votes[candidates_names] = 0
+    # Add a vote to that candidate's count
+      candidates_votes[candidates_names] +=1
 
-     #for row in file_reader:
-      # print(row)
+    #Iterate through the candidate list
+for candidates_names in candidates_votes: 
+    #Retrieve vote count of a candidate 
+    votes = candidates_votes[candidates_names]
+    #Calculate the percentage of votes 
+    vote_percentage = float(votes) / float(total_votes) * 100
+    #Print the candidate name and percentage of votes 
+    print(f"{candidates_names}: {vote_percentage:.1f}% ({votes:,})\n")
 
-#Read the file object with the reader function.
-  #file_reader = csv.reader(election_data)
+    # Determine winning vote count and candidate
+    # Determine if the votes is greater than the winning count.
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+         # If true then set winning_count = votes and winning_percent =
+         # vote_percentage.
+         winning_count = votes
+         winning_percentage = vote_percentage
+         # And, set the winning_candidate equal to the candidate's name.
+         winning_candidate = candidates_names
 
-#Print each row in the CSV file.
+winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
 
-
-#Create a filename to variable to a direct or or indirect path to the file. 
-
-#file_to_save = os.path.join(
-
-# Use the open statement to open the file as a text file. 
-
-#outfile = open(file_to_save, 'w')
-
-#write some data to the file
-  
-  #txt_file.write('Counties in the Election\n')
-  #txt_file.write('--------------\n')
-  #txt_file.write('Arapahoe\nDenver\nJefferson')
-  
-  #txt_file.write('Denver, ')
-  #txt_file.write('Jefferson, ')
-
-#close the file
-#outfile.close()
-
-#Open the election results and read the file.
-#with open(file_to_load) as election_data:
-#election_data = open(file_to_load, 'r')
-
-#To do: perform analysis. 
-  # print (election_data)
-
-#Close the file
-#election_data.close()
-
-# Use the now() attribute on the datetime class to get the present time.
-# now = dt.datetime.now()
-#Print the present time
-# print ('The time right now is', now#
-#
+print(winning_candidate_summary)
